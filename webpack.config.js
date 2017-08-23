959ff11afa8484e2bb6bb6f86c893a2af723d289
Andy Lien <andyhlien@gmail.com>
2017-08-23 14:45:49 -0700

    Bug fixed, options/query cannot be used with loaders

--- webpack.config.js
+++ webpack.config.js
@@ -21,16 +21,10 @@ module.exports = {
       {
         test : /\.jsx?/,
         include : SRC_DIR,
-        loader : ['babel-loader', 'file'],
+        loader : 'babel-loader',
         query: {
           presets: ['react', 'es2015']
        }
-      }, {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        loaders: [
-          'file?hash=sha512&digest=hex&name=[hash].[ext]',
-          'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
-        ]
       }
     ]
   }
