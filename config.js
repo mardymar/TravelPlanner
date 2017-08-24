e272984b7111f68a5ce23f0574be865e2858bd6e
Andy Lien <andyhlien@gmail.com>
2017-08-24 15:25:21 -0700

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports.flights = 'AIzaSyDs5BBUfhJVXBkHqXnazgLAP88bMh9Ci-s';
-module.exports.APCAuth = 'c7b1a553f5';
-module.exports.APCSecret = '4dfb93ab939f969';
-module.exports.yelpKey = 'Ihk3n8AkXwcAtQ0JvDu-1UVNs0dSA_TgQs7qfE79IH7B9EPDJinvdMxYvTla5ZvreNAoti1rU1xeJuba3h64TYtmHLzyJm2i96U4STiPJwfK1oiiOdFvofWZD-ycWXYx';
-module.exports.hotelkey = 'Ihk3n8AkXwcAtQ0JvDu-1UVNs0dSA_TgQs7qfE79IH7B9EPDJinvdMxYvTla5ZvreNAoti1rU1xeJuba3h64TYtmHLzyJm2i96U4STiPJwfK1oiiOdFvofWZD-ycWXYx';
-module.exports.darkskyAPI = 'd1537ecbe801c7f7618b0776d1a4b7eb';
-module.exports.geolocationAPI = 'AIzaSyBgAvUDLyE35GQ_ZaVkGBk4fiC1SdRGT5U';
